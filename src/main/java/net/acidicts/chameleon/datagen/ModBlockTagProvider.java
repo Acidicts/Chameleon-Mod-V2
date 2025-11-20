0858ce43d1bf8fd911cc64984956578b2ab73bca
Acidicts <alexjhayes1@hotmail.com>
2025-11-20 20:15:01 +0000

    Added cooked chameleon item and related recipes; introduced chameleon scale tools and updated item group Upload 2

--- src/main/java/net/acidicts/chameleon/datagen/ModBlockTagProvider.java
+++ src/main/java/net/acidicts/chameleon/datagen/ModBlockTagProvider.java
@@ -1,10 +1,13 @@
 package net.acidicts.chameleon.datagen;
 
 import net.acidicts.chameleon.block.ModBlocks;
+import net.acidicts.chameleon.item.ModItems;
+import net.acidicts.chameleon.util.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -21,5 +24,8 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
         getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
                 .add(ModBlocks.INCUBATOR);
+
+        getOrCreateTagBuilder(ModTags.Blocks.NEEDS_CHAMELEON_SCALE_TOOL)
+                .addTag(BlockTags.NEEDS_IRON_TOOL);
     }
 }
--- src/main/java/net/acidicts/chameleon/datagen/ModItemTagProvider.java
+++ src/main/java/net/acidicts/chameleon/datagen/ModItemTagProvider.java
@@ -4,6 +4,7 @@ import net.acidicts.chameleon.item.ModItems;
 import net.acidicts.chameleon.util.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
 
@@ -27,5 +28,57 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
                 .add(ModItems.CHAMELEON_SCALE_CHESTPLATE)
                 .add(ModItems.CHAMELEON_SCALE_LEGGINGS)
                 .add(ModItems.CHAMELEON_SCALE_BOOTS);
+
+        getOrCreateTagBuilder(ItemTags.ARMOR_ENCHANTABLE)
+                .add(ModItems.CHAMELEON_SCALE_HELMET)
+                .add(ModItems.CHAMELEON_SCALE_CHESTPLATE)
+                .add(ModItems.CHAMELEON_SCALE_LEGGINGS)
+                .add(ModItems.CHAMELEON_SCALE_BOOTS);
+
+        getOrCreateTagBuilder(ItemTags.HEAD_ARMOR_ENCHANTABLE)
+                .add(ModItems.CHAMELEON_SCALE_HELMET);
+
+        getOrCreateTagBuilder(ItemTags.CHEST_ARMOR_ENCHANTABLE)
+                .add(ModItems.CHAMELEON_SCALE_CHESTPLATE);
+
+        getOrCreateTagBuilder(ItemTags.LEG_ARMOR_ENCHANTABLE)
+                .add(ModItems.CHAMELEON_SCALE_LEGGINGS);
+
+        getOrCreateTagBuilder(ItemTags.FOOT_ARMOR_ENCHANTABLE)
+                .add(ModItems.CHAMELEON_SCALE_BOOTS);
+
+        getOrCreateTagBuilder(ItemTags.SWORDS)
+                .add(ModItems.CHAMELEON_SCALE_SWORD);
+        getOrCreateTagBuilder(ItemTags.PICKAXES)
+                .add(ModItems.CHAMELEON_SCALE_PICKAXE);
+        getOrCreateTagBuilder(ItemTags.AXES)
+                .add(ModItems.CHAMELEON_SCALE_AXE);
+        getOrCreateTagBuilder(ItemTags.SHOVELS)
+                .add(ModItems.CHAMELEON_SCALE_SHOVEL);
+        getOrCreateTagBuilder(ItemTags.HOES)
+                .add(ModItems.CHAMELEON_SCALE_HOE);
+
+        getOrCreateTagBuilder(ItemTags.WEAPON_ENCHANTABLE)
+                .add(ModItems.CHAMELEON_SCALE_SWORD)
+                .add(ModItems.CHAMELEON_SCALE_AXE);
+
+        getOrCreateTagBuilder(ItemTags.SWORDS)
+                .add(ModItems.CHAMELEON_SCALE_SWORD);
+
+        getOrCreateTagBuilder(ItemTags.DURABILITY_ENCHANTABLE)
+                .add(ModItems.CHAMELEON_SCALE_SWORD)
+                .add(ModItems.CHAMELEON_SCALE_PICKAXE)
+                .add(ModItems.CHAMELEON_SCALE_AXE)
+                .add(ModItems.CHAMELEON_SCALE_SHOVEL)
+                .add(ModItems.CHAMELEON_SCALE_HOE);
+
+        getOrCreateTagBuilder(ItemTags.MINING_ENCHANTABLE)
+                .add(ModItems.CHAMELEON_SCALE_PICKAXE)
+                .add(ModItems.CHAMELEON_SCALE_AXE)
+                .add(ModItems.CHAMELEON_SCALE_SHOVEL)
+                .add(ModItems.CHAMELEON_SCALE_HOE);
+
+        getOrCreateTagBuilder(ItemTags.MINING_LOOT_ENCHANTABLE)
+                .add(ModItems.CHAMELEON_SCALE_PICKAXE);
     }
 }
