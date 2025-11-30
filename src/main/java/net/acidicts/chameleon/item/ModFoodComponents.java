0eeadfaf69d275d8cd4f4ca2a60f4a006f70c5f5
Acidicts <alexjhayes1@hotmail.com>
2025-11-30 21:07:01 +0000

    Adjusted Golden Chameleon Effect Times

--- src/main/java/net/acidicts/chameleon/item/ModFoodComponents.java
+++ src/main/java/net/acidicts/chameleon/item/ModFoodComponents.java
@@ -9,7 +9,7 @@ public class ModFoodComponents {
     public static final FoodComponent GOLDEN_CHAMELEON = new FoodComponent.Builder().nutrition(10).saturationModifier(1.2F)
             .statusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY, 200), 0.7f)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 1000, 1), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 1000, 1), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 10000, 5), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 600, 1), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 1000, 5), 0.2f)
             .alwaysEdible().build();
 }
